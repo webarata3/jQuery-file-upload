eac4a14b77a10b14141865953cb2a4a4725484b1
webarata <shinichi@arata.link>
2015-11-25 05:03:50 +0900

    fix #1 Javaのソースのタブをスペースに変更

--- src/main/java/link/arata/fileupload/UploadServlet.java
+++ src/main/java/link/arata/fileupload/UploadServlet.java
@@ -11,37 +11,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-@WebServlet(name = "Upload", urlPatterns = {"/upload"})
-@MultipartConfig(fileSizeThreshold = 5000000, maxFileSize = 700 * 1024
-		* 1024, location = "/Users/arata/Desktop")
+@WebServlet(name = "Upload", urlPatterns = { "/upload" })
+@MultipartConfig(fileSizeThreshold = 5000000, maxFileSize = 700 * 1024 * 1024, location = "/Users/arata/Desktop")
 public class UploadServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		request.setCharacterEncoding("utf-8");
-		Part part = request.getPart("file");
-		String name = getFilename(part);
-		part.write(name);
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setCharacterEncoding("utf-8");
+        Part part = request.getPart("file");
+        String name = getFilename(part);
+        part.write(name);
 
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/json");
 
-		PrintWriter pw = response.getWriter();
-		pw.println("{\"result\":\"ok\"}");
-		pw.flush();
-		pw.close();
-	}
+        PrintWriter pw = response.getWriter();
+        pw.println("{\"result\":\"ok\"}");
+        pw.flush();
+        pw.close();
+    }
 
-	private String getFilename(Part part) {
-		for (String cd : part.getHeader("Content-Disposition").split(";")) {
-			if (cd.trim().startsWith("filename")) {
-				return cd.substring(cd.indexOf('=') + 1).trim().replace("\"",
-						"");
-			}
-		}
+    private String getFilename(Part part) {
+        for (String cd : part.getHeader("Content-Disposition").split(";")) {
+            if (cd.trim().startsWith("filename")) {
+                return cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
